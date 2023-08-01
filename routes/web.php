1173f5af8171801e584d48be38b5e7d64109f8df
tenyuoooogi <tanjoubi0103@gmail.com>
2023-08-01 23:05:10 +0900

    use 無　ビュー表示されなかった

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('products', ProductController::class);
+
 
 
 Auth::routes();
