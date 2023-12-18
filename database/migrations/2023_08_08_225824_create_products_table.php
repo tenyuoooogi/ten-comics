5b56e090ba74ddc5a9033824c89076b20767ce07
tenyuoooogi <tanjoubi0103@gmail.com>
2023-12-18 14:44:05 +0900

    作業中 user反映されない

--- database/migrations/2023_08_08_225824_create_products_table.php
+++ database/migrations/2023_08_08_225824_create_products_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->integer('category_id');
-
             $table->timestamps();
         });
     }
